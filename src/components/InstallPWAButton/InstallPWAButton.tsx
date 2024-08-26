ad620200872208d34ac1a7d7377ef82ee17fabc8
Debayan Pradhan <pradhandebayan@gmail.com>
2024-08-26 13:17:40 +0530

    install button test

---
+++ src/components/InstallPWAButton/InstallPWAButton.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import React, { useEffect, useState } from 'react'
+
+type BeforeInstallPromptEvent = Event & {
+  prompt: () => Promise<void>;
+  userChoice: Promise<{ outcome: 'accepted' | 'dismissed'; platform: string }>;
+};
+
+const InstallPWAButton = () => {
+
+  const  [prompt, setPrompt] = useState<BeforeInstallPromptEvent | null>(null)
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e : Event) => {
+        e.preventDefault()
+
+        setPrompt(e as BeforeInstallPromptEvent)
+
+        if(!window.matchMedia('(display-mode: standalone)').matches) {
+            console.log('The app is not installed')
+        }
+    })
+  }, [])
+    
+  return (
+    <div className='border-2 border-red-600 max-w-max rounded-lg'>
+        <button className='p-2' onClick={handleClick}>
+            Install App
+        </button>
+    </div>
+  )
+
+  function handleClick() {
+    if(prompt) {
+        prompt.prompt()
+    }
+  }
+}
+
+export default InstallPWAButton
