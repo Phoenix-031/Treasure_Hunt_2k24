4e6cbd0d9080f6d0123116fe8fe7fe6994ae14ca
Debayan Pradhan <pradhandebayan@gmail.com>
2024-08-31 18:33:32 +0530

    minor changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
 import { api } from "./query/api";
-import axios from "axios";
 
 export async function middleware(request :  NextRequest){
 
@@ -9,19 +8,17 @@ export async function middleware(request :  NextRequest){
     const teamId = request.nextUrl.pathname.split('/')[1]
     const questionId = request.nextUrl.pathname.split('/')[3]
 
-    console.log(request.nextUrl)
-
     const team = await api.get(`/team/${teamId}`)
     const teamData= team.data;
     const currentTeamStage = teamData.data.currentQuestionStage;
     const stageId = `q${teamData.data.currentQuestionStage}`
 
-    // if(currentTeamStage === null){
-    //     return NextResponse.redirect(new URL('/auth/login', request.url));
-    // }
-    // else if(stageId !== questionId){
-    //    return NextResponse.redirect(new URL(`/${teamId}/question/q${teamData.data.currentQuestionStage}`, request.url));
-    // }
+    if(currentTeamStage === null){
+        return NextResponse.redirect(new URL('/auth/login', request.url));
+    }
+    else if(stageId !== questionId){
+       return NextResponse.redirect(new URL(`/${teamId}/question/q${teamData.data.currentQuestionStage}`, request.url));
+    }
 
     return response;
 }
