46e1040110c7cb061b190f7dafc2f6a61599f278
Debayan Pradhan <pradhandebayan@gmail.com>
2024-09-05 02:25:30 +0530

    updates

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,42 +9,42 @@ export async function middleware(request :  NextRequest){
 
     const cookie = await verifySession();
 
-    // if(!cookie?.data) {
-    //     return NextResponse.redirect(new URL('/auth/login', request.url));
-    // }
+    if(!cookie?.data) {
+        return NextResponse.redirect(new URL('/auth/login', request.url));
+    }
 
-    // const questionId = request.nextUrl.pathname.split('/')[3]
+    const questionId = request.nextUrl.pathname.split('/')[3]
     // const teamId = request.nextUrl.pathname.split('/')[1]
-    // const teamId = (cookie?.data as { teamId: string }).teamId;
+    const teamId = (cookie?.data as { teamId: string }).teamId;
 
-    // const team = await api.get(`/team/${teamId}`)
-    // const teamData= team.data;
-    // const currentTeamStage = teamData.data.currentQuestionStage;
-    // const stageId = `q${teamData.data.currentQuestionStage}`
+    const team = await api.get(`/team/${teamId}`)
+    const teamData= team.data;
+    const currentTeamStage = teamData.data.currentQuestionStage;
+    const stageId = `q${teamData.data.currentQuestionStage}`
 
-    // if(request.nextUrl.pathname.split('/')[1] === 'startup') {
-    //     if(teamData.data.currentQuestionStage !== 0)
-    //       return NextResponse.redirect(new URL(`/${teamId}/question/q${teamData.data.currentQuestionStage}`, request.url));
-    // }else {
-
-        // if(teamData.data.currentQuestionStage === -1) {
-        //     return NextResponse.redirect(new URL('/complete', request.url));
-        // }
+    if(request.nextUrl.pathname.split('/')[1] === 'startup') {
+        if(teamData.data.currentQuestionStage !== 0)
+          return NextResponse.redirect(new URL(`/${teamId}/question/q${teamData.data.currentQuestionStage}`, request.url));
+    }
+    else {
+        if(teamData.data.currentQuestionStage === -1) {
+            return NextResponse.redirect(new URL('/complete', request.url));
+        }
         
-        // if(teamData.data.isDisqualified || teamData.data.numberOfLives === -1){
-        //     return NextResponse.redirect(new URL('/dead', request.url));
-        // }
+        if(teamData.data.isDisqualified || teamData.data.numberOfLives === -1){
+            return NextResponse.redirect(new URL('/dead', request.url));
+        }
 
-        // if(currentTeamStage === null){
-        //     return NextResponse.redirect(new URL('/auth/login', request.url));
-        // }
-        // else if(stageId !== questionId){
-        //     if(teamData.data.currentQuestionStage === 0){
-        //         return NextResponse.redirect(new URL('/startup', request.url));
-        //     }
-        // }
+        if(currentTeamStage === null){
+            return NextResponse.redirect(new URL('/auth/login', request.url));
+        }
+        else if(stageId !== questionId){
+            if(teamData.data.currentQuestionStage === 0){
+                return NextResponse.redirect(new URL('/startup', request.url));
+            }
+        }
         
-    // }
+    }
 
     return response;
 }
