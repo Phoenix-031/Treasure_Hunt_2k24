877c48c8b172d350205956a04bb0e065f326d584
Debayan Pradhan <pradhandebayan@gmail.com>
2024-09-04 23:23:28 +0530

    404 page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+        text: 'var(--text-color)',
+        background: 'var(--background-color)',
+        'button-bg': 'var(--button-background-color)',
+        'img-bg': 'var(--img-background)',
+        border: 'var(--border-color)',
+        shadow: 'var(--shadow-color)',
+        accent: 'var(--accent-color)',
+      },
     },
   },
   plugins: [],
