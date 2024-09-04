658c5deada61f3749c37f2745df58ec88b5309e5
Debayan Pradhan <pradhandebayan@gmail.com>
2024-09-05 02:16:35 +0530

    changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,36 +13,36 @@ export async function middleware(request :  NextRequest){
     //     return NextResponse.redirect(new URL('/auth/login', request.url));
     // }
 
-    const questionId = request.nextUrl.pathname.split('/')[3]
-    const teamId = request.nextUrl.pathname.split('/')[1]
+    // const questionId = request.nextUrl.pathname.split('/')[3]
+    // const teamId = request.nextUrl.pathname.split('/')[1]
     // const teamId = (cookie?.data as { teamId: string }).teamId;
 
-    const team = await api.get(`/team/${teamId}`)
-    const teamData= team.data;
-    const currentTeamStage = teamData.data.currentQuestionStage;
-    const stageId = `q${teamData.data.currentQuestionStage}`
+    // const team = await api.get(`/team/${teamId}`)
+    // const teamData= team.data;
+    // const currentTeamStage = teamData.data.currentQuestionStage;
+    // const stageId = `q${teamData.data.currentQuestionStage}`
 
     // if(request.nextUrl.pathname.split('/')[1] === 'startup') {
     //     if(teamData.data.currentQuestionStage !== 0)
     //       return NextResponse.redirect(new URL(`/${teamId}/question/q${teamData.data.currentQuestionStage}`, request.url));
     // }else {
 
-        if(teamData.data.currentQuestionStage === -1) {
-            return NextResponse.redirect(new URL('/complete', request.url));
-        }
+        // if(teamData.data.currentQuestionStage === -1) {
+        //     return NextResponse.redirect(new URL('/complete', request.url));
+        // }
         
-        if(teamData.data.isDisqualified || teamData.data.numberOfLives === -1){
-            return NextResponse.redirect(new URL('/dead', request.url));
-        }
+        // if(teamData.data.isDisqualified || teamData.data.numberOfLives === -1){
+        //     return NextResponse.redirect(new URL('/dead', request.url));
+        // }
 
-        if(currentTeamStage === null){
-            return NextResponse.redirect(new URL('/auth/login', request.url));
-        }
-        else if(stageId !== questionId){
-            if(teamData.data.currentQuestionStage === 0){
-                return NextResponse.redirect(new URL('/startup', request.url));
-            }
-        }
+        // if(currentTeamStage === null){
+        //     return NextResponse.redirect(new URL('/auth/login', request.url));
+        // }
+        // else if(stageId !== questionId){
+        //     if(teamData.data.currentQuestionStage === 0){
+        //         return NextResponse.redirect(new URL('/startup', request.url));
+        //     }
+        // }
         
     // }
 
